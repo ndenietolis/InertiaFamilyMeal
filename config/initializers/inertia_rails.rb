5cacc7fe0bda3e0ad33ef61459498c82ce8c7b32
Nick Denietolis <ndenietolis@gmail.com>
2026-02-26 16:46:28 -0500

    Add ssr_url to initializer

--- config/initializers/inertia_rails.rb
+++ config/initializers/inertia_rails.rb
@@ -7,4 +7,5 @@ InertiaRails.configure do |config|
   config.use_script_element_for_initial_page = true
   config.use_data_inertia_head_attribute = true
   config.ssr_enabled = Rails.env.production?
+  config.ssr_url = ENV.fetch("INERTIA_SSR_URL", "http://localhost:13714")
 end
